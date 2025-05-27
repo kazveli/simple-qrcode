1b7ab1af3c459488ffe43e2ea500f0ea6c2f615c
Kaique <kaostech.naru@gmail.com>
2025-05-26 21:13:42 -0300

    Adicionado comentários e sofisticando logotipo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple QRCode encryptor
+Usage: main.py <all-data-to-be-encrypted>
+
+#### If you want to set a logo, justo take a png image and rename it to logotype.png
--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from PIL import Image
-import qrcode
+import qrcode, sys
 
 #Cria o objeto
-qr = qrcode.QRCode(version=10, box_size=10, border=2)
+qr = qrcode.QRCode(
+    version=10,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4
+)
 
 #Define os dados do qrcode
-data = "https://www.example.com/"
+data = " ".join(sys.argv[1:])
 qr.add_data(data)
 
 #Monta o qrcode
 qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white")
 
 #Logotipo
-logo = Image.open("logotipo.png")
-logo = logo.resize((125,125))
-img_w, img_h = img.size 
+logo = Image.open("logotype.png")
+logo = logo.convert("RGB")
+base_width = qr.modules_count * 5
+logo = logo.resize((int(base_width / 3), int(base_width /3)))
+
 logo_w, logo_h = logo.size 
-pos = ((img_w - logo_w) // 2, (img_h - logo_h) // 2)
-img.paste(logo,pos)
+img = qr.make_image().convert("RGB")
+img_w, img_h = img.size
+offset_x = (img_w - logo_w) // 2
+offset_y = (img_h - logo_h) // 2
+img.paste(logo, (offset_x, offset_y))
 
 #Salve o qrcode
-img.save("qr-code.png")
+img.show()
